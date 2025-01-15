06294470b2bfb6c257ec5e6ee72b7e8fc16f0e75
RecepG314 <91325556+RecepG314@users.noreply.github.com>
2025-01-15 16:49:00 +0300

    Delete deneme directory

--- deneme/deneme.py
+++
@@ -1,8 +0,0 @@
-import torch
-import torchvision.models as models
-
-model = models.resnet50(pretrained=True).eval()
-x = torch.rand(1, 3, 224, 224)  # Örnek giriş
-with torch.no_grad():
-    features = model(x)
-print(features.shape)
